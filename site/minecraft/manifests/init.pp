dd130bd544d9a8e70aafc765799e1466d6831f17
jerrycan321 <jerrycan321@gmail.com>
2019-09-25 16:51:32 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,7 +1,9 @@
-class minecraft {
+class minecraft (
   $url = 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
   $install_dir = '/opt/minecraft'
   
+
+  ) {
   file {'/opt/minecraft':
     ensure => directory,
   }
