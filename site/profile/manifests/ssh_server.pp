48b2bb93547e1c756d7fa308dd5e4378ac23f3bc
jerrycan321 <jerrycan321@gmail.com>
2019-09-25 11:18:53 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
    ensure  => present,
    user    => 'root',
    type    => 'ssh-rsa',
-   key     => '',
+   key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDnmP/l3gyLL2JmjdU0tLG9GJ4q5lJUxev0Ga4srx08GzyG57BMmri2Xfj1he4JU09+GoNVbz8+wVuZMI8JFqXCONrqu1x4uRadC3gZu8PEcpgF8o9u/hzRGKSSbQjl8I5AbNHcD2vkdlBVAm0BSSLbqOcMJqULuBWmTCYdn7e6Whrfs1IeFoAOb3OWmhPC7V8g34+AmxO0P4Aqf+n8y/1MeloGj7CVlYAZLEMWgxxZ2WS+guhBm7ueYv3kCwikIuIW3yW/Md8G/1P60hHGs98pejQ8nACgJLr56YbRzwHAhKMFkL1jU3yk6jOv6uImwmvIN5O5Kpt0XJuY19gXWBtr',
   }
 }
